0b7230ec371d10b1024dbaed221beffb900b0581
Damodar <damodarms0804@gmail.com>
2026-06-07 16:30:04 +0545

    Add missing 'use client' directive to HeroSearch.tsx

--- components/HeroSearch.tsx
+++ components/HeroSearch.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { FaSearch, FaClock, FaDollarSign, FaMapMarkerAlt, FaChevronDown } from "react-icons/fa";
